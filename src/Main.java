df8d94218c9685c6221dd68527bf5e0ef17c2cb6
CodeWithCompound <mathematico.loco@gmail.com>
2025-12-25 21:57:03 +0100

    removing unwanted comments

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+
+        unsorted_nummbers = new int[5];
+
+        unsorted_nummbers[0] = 26;
+        unsorted_nummbers[1] = 160;
+        unsorted_nummbers[2] = 2;
+        unsorted_nummbers[3] = 29;
+        unsorted_nummbers[4] = 1;
 
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
     }
