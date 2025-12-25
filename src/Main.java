7cdbb3e3adcf0d2da2f1f66e659d7f4f990bd717
CodeWithCompound <mathematico.loco@gmail.com>
2025-12-25 21:58:24 +0100

    declaring the array

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        int unsorted_numbers[];
+        unsorted_numbers = new int[5];
 
-        unsorted_nummbers = new int[5];
-
-        unsorted_nummbers[0] = 26;
-        unsorted_nummbers[1] = 160;
-        unsorted_nummbers[2] = 2;
-        unsorted_nummbers[3] = 29;
-        unsorted_nummbers[4] = 1;
+        unsorted_numbers[0] = 26;
+        unsorted_numbers[1] = 160;
+        unsorted_numbers[2] = 2;
+        unsorted_numbers[3] = 29;
+        unsorted_numbers[4] = 1;
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
